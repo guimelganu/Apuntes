ca5b80497b06edbac5dc959034ce5236beb7283b
guimelganu <guimelgn96@hotmail.com>
2022-06-25 14:07:28 -0500

    Upload

---
+++ Pininos NodeJs/main.js
@@ -0,0 +1,13 @@
+/* function suma(a,b) {
+    return a + b;
+}
+
+console.log(suma(3, 4));
+console.log(suma(7, 8));
+console.log(suma(498, 400)); */
+
+function saludar (nombre) {
+    console.log(`Hola ${nombre} como estas?`);
+}
+
+saludar('Andres');
---
+++ Pininos NodeJs/operaciones/index.js
@@ -0,0 +1,7 @@
+const {resta} = require("./resta");
+const importacion = require("./suma");
+const operacionMulti = require("./multiplicacion");
+
+console.log(importacion.suma(5, 3));
+console.log(operacionMulti(15, 8));
+console.log(resta(8,9));
